3a228a1332446f44431b19fa2e5cf3b710aa1ba1
Yuliya Pavlovich <juli.pavlovicvh@gmail.com>
2019-02-03 01:45:33 +0300

    Creating auxiliary methods, improving the "readability" of the code, adding comments

--- test_add_group.py
+++ test_add_group.py
@@ -1,28 +1,35 @@
 # -*- coding: utf-8 -*-
+# #используем псевдокомментарий чтобы можно было использовать не латинские символы
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
-class test_add_group(unittest.TestCase):
+
+class test_add_group(unittest.TestCase):  # функ-я инициализации, вып-ся ДО рана теста,напр. можем запустить браузер
     def setUp(self):
-        self.wd = webdriver.Firefox()
+        self.wd = webdriver.Firefox()  # передали ссылку на драйвер
         self.wd.implicitly_wait(30)
-
     
     def test_add_group(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        wd.find_element_by_link_text("groups").click()
+        wd = self.wd  # извлекли ссылку на драйвер
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_groups_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+    def create_group(self, wd):
+        # init group creation
         wd.find_element_by_name("new").click()
+        # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("testgroup1")
@@ -32,24 +39,25 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("testgroupfooter1")
+        # submit group creation
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Logout").click()
-    
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
 
+    def open_groups_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def login(self, wd):
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/index.php")
     
-    def tearDown(self):
+    def tearDown(self):  # функция зачистки
         self.wd.quit()
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # если явно не указывать pytest
     unittest.main()
