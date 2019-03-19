3ee04b5a2c2fc90f58efd1ca8223b815d38188e5
Yuliya Pavlovich <juli.pavlovicvh@gmail.com>
2019-03-20 00:54:34 +0300

    Доработан тест для добавления групп (сравнение самих списков)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,14 +5,20 @@ from model.group import Group
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()  # сохраняем старый список групп
-    app.group.create(Group(name="testgroup1", header="testgroupheader1", footer="testgroupfooter1"))
+    group = Group(name="testgroup1", header="testgroupheader1", footer="testgroupfooter1")
+    app.group.create(group)
     # app.session.logout()
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_add_empty_group(app):
     old_groups = app.group.get_group_list()  # сохраняем старый список групп
-    app.group.create(Group(name="", header="", footer=""))
+    group = Group(name="", header="", footer="")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
