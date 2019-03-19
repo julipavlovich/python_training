0d8a6db41fdd4500248e4f37836a887d73ee0116
Yuliya Pavlovich <juli.pavlovicvh@gmail.com>
2019-03-20 00:11:04 +0300

    Сравнение длины списков для тестов групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,9 +4,15 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()  # сохраняем старый список групп
     app.group.create(Group(name="testgroup1", header="testgroupheader1", footer="testgroupfooter1"))
     # app.session.logout()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()  # сохраняем старый список групп
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
--- test/test_modif_group.py
+++ test/test_modif_group.py
@@ -4,28 +4,41 @@ from model.group import Group
 def test_modify_group_to_empty(app):
     if app.group.count() == 0:
         app.group.create(Group(name="name1", header="header1", footer="footer1"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="", header="", footer=""))
 
 
 def test_modify_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="name1", header="header1", footer="footer1"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="newname1", header="newheader1", footer="newfooter1"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="name1", header="header1", footer="footer1"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="New name"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="name1", header="header1", footer="footer1"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(header="New header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_footer(app):
     if app.group.count() == 0:
         app.group.create(Group(name="name1", header="header1", footer="footer1"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(footer="New footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
