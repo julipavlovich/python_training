e0281f5d777cfd809944144c707068470c5ab447
Yuliya Pavlovich <juli.pavlovicvh@gmail.com>
2019-03-20 17:55:02 +0300

    Хэширование и кеширование для теста создания  групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -8,17 +8,17 @@ def test_add_group(app):
     group = Group(name="testgroup1", header="testgroupheader1", footer="testgroupfooter1")
     app.group.create(group)
     # app.session.logout()
+    assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()  # сохраняем старый список групп
-    group = Group(name="", header="", footer="")
-    app.group.create(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+# def test_add_empty_group(app):
+#    old_groups = app.group.get_group_list()  # сохраняем старый список групп
+#    group = Group(name="", header="", footer="")
+#    app.group.create(group)
+#    new_groups = app.group.get_group_list()
+#    assert len(old_groups) + 1 == len(new_groups)
+#    old_groups.append(group)
+#    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
