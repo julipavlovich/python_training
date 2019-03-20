cb10fea18e40267844a01b0b6e1aa06d62135048
Yuliya Pavlovich <juli.pavlovicvh@gmail.com>
2019-03-20 18:02:09 +0300

    Хэширование и кэширование для удаления и модификации групп

--- test/test_modif_group.py
+++ test/test_modif_group.py
@@ -1,17 +1,17 @@
 from model.group import Group
 
 
-def test_modify_group_to_empty(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="name1", header="header1", footer="footer1"))
-    old_groups = app.group.get_group_list()
-    group = Group(name="", header="", footer="")
-    group.id = old_groups[0].id
-    app.group.modify_first_group(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
-    old_groups[0] = group
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+# def test_modify_group_to_empty(app):
+#    if app.group.count() == 0:
+#        app.group.create(Group(name="name1", header="header1", footer="footer1"))
+#    old_groups = app.group.get_group_list()
+#    group = Group(name="", header="", footer="")
+#    group.id = old_groups[0].id
+#    app.group.modify_first_group(group)
+#    new_groups = app.group.get_group_list()
+#    assert len(old_groups) == len(new_groups)
+#    old_groups[0] = group
+#    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_modify_group(app):
@@ -21,23 +21,23 @@ def test_modify_group(app):
     group = Group(name="newname1", header="newheader1", footer="newfooter1")
     group.id = old_groups[0].id
     app.group.modify_first_group(group)
+    assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
     old_groups[0] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_modify_group_name(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="name1", header="header1", footer="footer1"))
-    old_groups = app.group.get_group_list()
-    group = Group(name="New name")
-    group.id = old_groups[0].id
-    app.group.modify_first_group(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
-    old_groups[0] = group
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+# def test_modify_group_name(app):
+#    if app.group.count() == 0:
+#        app.group.create(Group(name="name1", header="header1", footer="footer1"))
+#    old_groups = app.group.get_group_list()
+#    group = Group(name="New name")
+#    group.id = old_groups[0].id
+#    app.group.modify_first_group(group)
+#    new_groups = app.group.get_group_list()
+#    assert len(old_groups) == len(new_groups)
+#    old_groups[0] = group
+#    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 # def test_modify_group_header(app):
