d001daa0dcae26a9de08468de6663b789a54fb05
Yuliya Pavlovich <juli.pavlovicvh@gmail.com>
2019-03-22 02:43:11 +0300

    Доработка тестов по модификации контактов (сравнение списков)

--- test/test_modif_contact.py
+++ test/test_modif_contact.py
@@ -4,22 +4,29 @@ from model.contact import Contact
 def test_modify_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="NewFirstName1", lastname="NewLastName1"))
-    app.contact.modify_first_contact(Contact(firstname="NewFirstName1", lastname="NewLastName1"))
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname="NewFirstName1", lastname="NewLastName1")
+    contact.id = old_contacts[0].id
+    app.contact.modify_first_contact(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
-def test_modify_contact_to_empty(app):
-    if app.contact.count() == 0:
-        app.contact.create(Contact(firstname="NewFirstName1", lastname="NewLastName1"))
-    app.contact.modify_first_contact(Contact(firstname="", lastname=""))
+# def test_modify_contact_to_empty(app):
+#   if app.contact.count() == 0:
+#        app.contact.create(Contact(firstname="NewFirstName1", lastname="NewLastName1"))
+#    app.contact.modify_first_contact(Contact(firstname="", lastname=""))
 
 
-def test_modify_contact_firstname(app):
-    if app.contact.count() == 0:
-        app.contact.create(Contact(firstname="NewFirstName1", lastname="NewLastName1"))
-    app.contact.modify_first_contact(Contact(firstname="New First Name"))
+# def test_modify_contact_firstname(app):
+#    if app.contact.count() == 0:
+#        app.contact.create(Contact(firstname="NewFirstName1", lastname="NewLastName1"))
+#    app.contact.modify_first_contact(Contact(firstname="New First Name"))
 
 
-def test_modify_contact_lastname(app):
-    if app.contact.count() == 0:
-        app.contact.create(Contact(firstname="NewFirstName1", lastname="NewLastName1"))
-    app.contact.modify_first_contact(Contact(lastname="New Last Name"))
+# def test_modify_contact_lastname(app):
+#    if app.contact.count() == 0:
+#        app.contact.create(Contact(firstname="NewFirstName1", lastname="NewLastName1"))
+#    app.contact.modify_first_contact(Contact(lastname="New Last Name"))
