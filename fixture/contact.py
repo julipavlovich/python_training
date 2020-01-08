e20ccaa56281e7cbb0489e84939de27cebcd9d56
Yuliya Pavlovich <juli.pavlovicvh@gmail.com>
2020-01-08 04:16:49 +0300

    Проверка адресов электронной почты
    тест, который проверяет, что для некоторого одного случайно выбранного контакта адреса электронной почты на главной странице (в таблице) соответствует информации, представленной в форме редактирования контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -108,8 +108,9 @@ class ContactHelper:
                 f_name = cells[2].text
                 l_name = cells[1].text
                 all_phones = cells[5].text
+                all_emails = cells[4].text
                 address = cells[3].text
-                self.contact_cache.append(Contact(id=id, all_phones_from_home_page=all_phones, lastname=l_name, firstname=f_name, address=address))
+                self.contact_cache.append(Contact(id=id, all_phones_from_home_page=all_phones, all_emails_from_home_page=all_emails, lastname=l_name, firstname=f_name, address=address))
         return list(self.contact_cache)
 
         # for row in wd.find_elements_by_css_selector("tr[name='entry']"):
@@ -143,7 +144,10 @@ class ContactHelper:
         mobilephone = wd.find_element_by_name("mobile").get_attribute("value")
         secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
         address = wd.find_element_by_name("address").get_attribute("value")
-        return Contact(firstname=firstname, lastname=lastname, id=id, homephone=homephone, workphone=workphone, mobilephone=mobilephone, secondaryphone=secondaryphone, address=address)
+        email1 = wd.find_element_by_name("email").get_attribute("value")
+        email2 = wd.find_element_by_name("email2").get_attribute("value")
+        email3 = wd.find_element_by_name("email3").get_attribute("value")
+        return Contact(firstname=firstname, lastname=lastname, id=id, homephone=homephone, workphone=workphone, mobilephone=mobilephone, secondaryphone=secondaryphone, address=address, email1=email1, email2=email2, email3=email3)
 
     def open_contact_view_by_index(self, index):
         wd = self.app.wd
--- model/contact.py
+++ model/contact.py
@@ -3,7 +3,7 @@ from sys import maxsize
 
 class Contact:
 
-    def __init__(self, firstname=None, lastname=None, homephone=None, mobilephone=None, workphone=None, secondaryphone=None, id=None, address=None, all_phones_from_home_page=None):
+    def __init__(self, firstname=None, lastname=None, homephone=None, mobilephone=None, workphone=None, secondaryphone=None, id=None, email1=None, email2 = None, email3=None, address=None, all_emails_from_home_page=None,  all_phones_from_home_page=None):
         self.firstname = firstname
         self.lastname = lastname
         self.homephone = homephone
@@ -13,6 +13,10 @@ class Contact:
         self.all_phones_from_home_page = all_phones_from_home_page
         self.id = id
         self.address = address
+        self.all_emails_from_home_page = all_emails_from_home_page
+        self.email1 = email1
+        self.email2 = email2
+        self.email3 = email3
 
     def __repr__(self):
         return "%s:%s:%s" % (str(self.id), self.firstname, self.lastname)
