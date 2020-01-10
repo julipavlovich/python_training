79af115f90ad2fe4ee7fce4ff505f9d47b793ec2
Yuliya Pavlovich <juli.pavlovicvh@gmail.com>
2020-01-10 04:16:38 +0300

    Параметризация создания контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,15 +1,26 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [
+    Contact(firstname=firstname, lastname=lastname)
+    for firstname in ["", random_string("fname", 20)]
+    for lastname in ["", random_string("lname", 20)]
+]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="FirstName1", lastname="LastName1")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-# def test_add_empty_contact(app):
-#    app.contact.create(Contact(firstname="", lastname=""))
