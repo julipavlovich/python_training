8a771a19b280c9350817c0ce3dc834396274f06b
Yuliya Pavlovich <juli.pavlovicvh@gmail.com>
2020-01-27 18:36:13 +0300

    Реализована загрузка информации о тестируемом приложении из конфигурационного файла

--- conftest.py
+++ conftest.py
@@ -1,23 +1,25 @@
 import pytest
+import json
+import os.path
 from fixture.application import Application
 
 # задали глобальную переменную
 fixture = None
+target = None
 
 
 @pytest.fixture
 def app(request):
-    # объявили глобальную переменную
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    if fixture is None:
-        # инициализация созд-я фикстуры
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username="admin", password="secret")
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target['baseUrl'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -33,4 +35,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/index.php")
+    parser.addoption("--target", action="store", default="target.json")
