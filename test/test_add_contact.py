896dd43e0da262ace01df6a2ec3afde211e1f3ae
Yuliya Pavlovich <juli.pavlovicvh@gmail.com>
2020-01-27 22:57:04 +0300

    Реализованы генераторы тестовых данных для контактов (json)

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,17 +5,6 @@ import random
 import string
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Contact(firstname="", lastname="")] + [
-    Contact(firstname=random_string("fname", 10), lastname=random_string("lname", 10))
-    for i in range(5)
-]
-
-
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
 def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
